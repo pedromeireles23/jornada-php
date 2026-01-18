31fb7882548263da6aa752a7fe97e3c570bdb334
pedromeireles23 <pemeirels@gmail.com>
2026-01-18 07:44:11 -0300

    Lógica da criação do usuário aplicando o filtro nos inputs e o hash na senha

--- sistemaDeCadastroDeUsuarios/index.php
+++ sistemaDeCadastroDeUsuarios/index.php
@@ -1,4 +1,6 @@
-
+<?php 
+include ("database.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +13,7 @@
     Sistema de Cadastro de Usuários
   </h1>
 
-  <form action="index.php" method="$_POST">
+  <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
     <label for="usuario">Usuário:</label>
     <input type="text" name="usuario" id="usuario" required>
     <br><br>
@@ -25,7 +27,24 @@
 
 <?php 
 
-include ("database.php");
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $usuario = filter_input(INPUT_POST, "usuario", FILTER_SANITIZE_SPECIAL_CHARS);
+  $senha = filter_input(INPUT_POST, "senha", FILTER_SANITIZE_SPECIAL_CHARS);
 
+  if(empty($usuario) || empty($senha)){
+    echo "Por favor, preencha todos os campos.";
+    
+  }else{
+    
+    $hashed_password = password_hash($senha, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO usuarios (usuario, senha) VALUES ('$usuario', '$hashed_password')";
+    mysqli_query($conn, $sql);
+    echo "Usuário cadastrado com sucesso!";
+  }
+
+ 
+}
+
+mysqli_close($conn);
 
 ?>
