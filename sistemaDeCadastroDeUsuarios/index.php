540ae7b8d938878ed1292e8c915248e819d55a20
pedromeireles23 <pemeirels@gmail.com>
2026-01-18 07:25:37 -0300

    Criação do form de cadastro

--- sistemaDeCadastroDeUsuarios/index.php
+++ sistemaDeCadastroDeUsuarios/index.php
@@ -1,10 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CrudSimples</title>
+</head>
+<body>
+  <h1>
+    Sistema de Cadastro de Usuários
+  </h1>
+
+  <form action="index.php" method="$_POST">
+    <label for="usuario">Usuário:</label>
+    <input type="text" name="usuario" id="usuario" required>
+    <br><br>
+    <label for="senha">Senha:</label>
+    <input type="password" name="senha" id="senha" required>
+    <br><br>
+    <input type="submit" value="Cadastrar">
+  </form>
+</body>
+</html>
+
 <?php 
 
 include ("database.php");
-echo "Bem-vindo ao Sistema de Cadastro de Usuários!";
-
-
-
 
 
 ?>
