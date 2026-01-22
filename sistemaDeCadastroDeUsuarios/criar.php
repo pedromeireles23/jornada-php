ff6879d401f2997c8932023d201ce3969d20bee6
pedromeireles23 <pemeirels@gmail.com>
2026-01-22 20:29:45 -0300

    Melhora na organização dos arquivos + implementação do Bootstrap no projeto..

---
+++ sistemaDeCadastroDeUsuarios/criar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro de Usuário</title>
+</head>
+<body>
+  <h1>
+    Sistema de Cadastro de Usuários
+  </h1>
+
+  <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+    <label for="usuario">Usuário:</label>
+    <input type="text" name="usuario" id="usuario" required>
+    <br><br>
+    <label for="senha">Senha:</label>
+    <input type="password" name="senha" id="senha" required>
+    <br><br>
+    <input type="submit" value="Cadastrar">
+  </form>
+<?php 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $usuario = filter_input(INPUT_POST, "usuario", FILTER_SANITIZE_SPECIAL_CHARS);
+  $senha = filter_input(INPUT_POST, "senha", FILTER_SANITIZE_SPECIAL_CHARS);
+
+  if(empty($usuario) || empty($senha)){
+    echo "Por favor, preencha todos os campos.";
+    
+  }else{
+    
+    $hashed_password = password_hash($senha, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO usuarios (usuario, senha) VALUES ('$usuario', '$hashed_password')";
+
+    try{
+      mysqli_query($conn, $sql);
+      echo "Usuário cadastrado com sucesso!";
+    }catch(mysqli_sql_exception){
+        echo "Erro: O nome de usuário já existe. Por favor, escolha outro.";
+    }
+  }
+
+ 
+}
+?>
+</body>
+</html>
--- sistemaDeCadastroDeUsuarios/index.php
+++ sistemaDeCadastroDeUsuarios/index.php
@@ -6,50 +6,56 @@ include ("database.php");
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   <title>CrudSimples</title>
+  
 </head>
 <body>
-  <h1>
-    Sistema de Cadastro de Usuários
-  </h1>
-
-  <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
-    <label for="usuario">Usuário:</label>
-    <input type="text" name="usuario" id="usuario" required>
-    <br><br>
-    <label for="senha">Senha:</label>
-    <input type="password" name="senha" id="senha" required>
-    <br><br>
-    <input type="submit" value="Cadastrar">
-  </form>
-</body>
-</html>
-
-<?php 
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-  $usuario = filter_input(INPUT_POST, "usuario", FILTER_SANITIZE_SPECIAL_CHARS);
-  $senha = filter_input(INPUT_POST, "senha", FILTER_SANITIZE_SPECIAL_CHARS);
-
-  if(empty($usuario) || empty($senha)){
-    echo "Por favor, preencha todos os campos.";
-    
-  }else{
-    
-    $hashed_password = password_hash($senha, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO usuarios (usuario, senha) VALUES ('$usuario', '$hashed_password')";
-
-    try{
-      mysqli_query($conn, $sql);
-      echo "Usuário cadastrado com sucesso!";
-    }catch(mysqli_sql_exception){
-        echo "Erro: O nome de usuário já existe. Por favor, escolha outro.";
-    }
-  }
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h1>Lista de Usuários
+            <a href="criar.php" class="btn btn-primary float-end">Adicionar Usuário</a>
+            </h1>
+          </div>
+          <div class="card-body">
 
  
-}
+  <table class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Data Cadastro</th>
+        <th>Ações</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <td>1</td>
+      <td>teste</td>
+      <td>Data</td>
+      <td>
+        <a href="#" class="btn btn-primary btn-sm">Editar</a>
+        
+        <form action="" method="POST" class="d-inline">
+          <button type="submit" value = "1" class="btn btn-danger btn-sm">Excluir</button>
+        </form>
+      </td>
+    </tr>
+   
+    </tbody>
+  </table>
+  </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
 
-mysqli_close($conn);
+</html>
 
-?>
