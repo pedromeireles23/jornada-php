f0197b804a87aed1bcd8629f1c83a75ee2a9afd1
pedromeireles23 <pemeirels@gmail.com>
2026-01-23 01:09:06 -0300

    Adição de validação de exclusão do usuário com js inline e exclui o arquivo excluir.php porque não tinha necessidade

--- sistemaDeCadastroDeUsuarios/index.php
+++ sistemaDeCadastroDeUsuarios/index.php
@@ -49,8 +49,8 @@ require ("database.php");
         echo '<td>
         <a href="editar.php?id='.$row['id'].' " class="btn btn-primary btn-sm">Editar</a>
         
-        <form action="acoes.php" method="POST" class="d-inline">
-          <button type="submit" name="deletar_usuario" value="'.$row['id'].'" class="btn btn-danger btn-sm">Excluir</button>
+        <form action="acoes.php" "method="POST" class="d-inline">
+          <button type="submit" onclick="return confirm(\'Tem certeza que deseja excluir?\')" name="deletar_usuario" value="' . $row['id'] . '" class="btn btn-danger btn-sm">Excluir</button>
         </form>
       </td>';
       echo "</tr>";
