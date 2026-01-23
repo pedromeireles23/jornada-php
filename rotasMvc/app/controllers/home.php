d9ee025767d8e1d3524147b2aa3c0024de147db8
pedromeireles23 <pemeirels@gmail.com>
2026-01-23 11:09:44 -0300

    mais alterações mais testes

--- rotasMvc/app/controllers/home.php
+++ rotasMvc/app/controllers/home.php
@@ -2,11 +2,11 @@
 
 class Home extends Controller {
   public function index($name = '') {
-    echo $name;
+    $user   = $this->model('User');
+    $user->name = $name;
+    $this->view('home/index', ['name' => $user->name]);
   }
-  public function test (){
-    echo 'teste';  
-  }
+  
 }
 
 ?>
